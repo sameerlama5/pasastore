fedef709e3c29207c6d9c2d89fbf15be996446cf
sameer tamang <tamansameer5@gmail.com>
2024-02-23 15:02:29 +0545

    first commit

---
+++ src/components/home/Footer/FooterBottom.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full bg-[#F5F5F3] group">
+      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
+        <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
+          <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
+          Copyright 2024 | PASA STORE | All Rights Reserved by Shreejan & Sameer | Powered by ABC.com
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBottom;
